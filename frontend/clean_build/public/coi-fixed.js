c65603ff02ec90981d609f0bbe91115f941381b7
tao-shen <tao.shen@zju.edu.cn>
2026-01-27 02:12:24 +0800

    feat: enable COEP credentialless mode in coi-serviceworker

--- frontend/clean_build/public/coi-fixed.js
+++ frontend/clean_build/public/coi-fixed.js
@@ -1,5 +1,5 @@
 /*! coi-serviceworker v0.1.7 - Guido Zuidhof, licensed under MIT */
-let coepCredentialless = false;
+let coepCredentialless = true;
 if (typeof window === 'undefined') {
   self.addEventListener("install", () => self.skipWaiting());
   self.addEventListener("activate", (event) => event.waitUntil(self.clients.claim()));
